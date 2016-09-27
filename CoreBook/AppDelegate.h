3da7499f9d88eb95887c5f83449f32b6e9fe370d
Adrian Perez Lopez <a.przlopez@gmail.com>
2016-09-27 18:29:38 +0200

    Creo proyecto y añado ficheros facilitados por Fernando.

---
+++ CoreBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreBook
+//
+//  Created by Adrian Perez Lopez on 27/9/16.
+//  Copyright © 2016 NovasFactory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
