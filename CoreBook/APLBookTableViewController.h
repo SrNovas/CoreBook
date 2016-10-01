e9c5b55480708e97357d6f24161227b39f519054
Adrian Perez Lopez <a.przlopez@gmail.com>
2016-10-01 12:07:55 +0200

    Añado BookTableViewController's.

---
+++ CoreBook/APLBookTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  APLBookTableViewController.h
+//  CoreBook
+//
+//  Created by Adrian Perez Lopez on 1/10/16.
+//  Copyright © 2016 NovasFactory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APLBookTableViewController : UITableViewController
+
+@end
