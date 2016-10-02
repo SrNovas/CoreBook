a21b80cb8f44db4bff01c028ce7a25cdcb9b7ae9
Adrian Perez Lopez <a.przlopez@gmail.com>
2016-10-02 22:21:08 +0200

    Falta por implementar el 2º método de BookTag.

--- CoreBook/APLBookTag.h
+++ CoreBook/APLBookTag.h
@@ -1,9 +1,17 @@
 #import "_APLBookTag.h"
 
+@class APLBook;
+@class APLTag;
+
 @interface APLBookTag : _APLBookTag
 
 +(instancetype) bookTagWithBook: (APLBook *) book
                             tag:(APLTag *) tag
                         context:(NSManagedObjectContext *) context;
+
+//Me falta por implementarlo
++(instancetype) bookTagWithName:(NSString*) name
+                           Book:(APLBook*) book
+                        context:(NSManagedObjectContext*) context;
 
 @end
